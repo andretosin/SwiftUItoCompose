dff6f256f92b8803ff3902f2f621324e28426fe0
André Luiz Tosin <andretosin@brq.com>
2023-08-01 13:51:16 -0300

    Initial Commit

---
+++ SwiftUItoCompose/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUItoCompose
+//
+//  Created by Andre on 01/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUItoCompose/SwiftUItoComposeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUItoComposeApp.swift
+//  SwiftUItoCompose
+//
+//  Created by Andre on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUItoComposeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
